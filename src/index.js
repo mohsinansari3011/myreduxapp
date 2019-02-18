b97f49ab808079edd29e1796d69cfcb9cdc771a5
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-02-18 22:04:25 +0500

    Reducers

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux'
 
 
 
-
+const createStoreWithMiddleware = applyMiddleware()(createStore)
 const App = () =>{
 
     return(<div>
-        
+        helllo
     </div>)
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={createStoreWithMiddleware()} > <App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
