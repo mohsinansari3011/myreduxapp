e6f38fd543810103639180ce686ace58621ec092
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-04-18 21:08:23 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const createStoreWithMiddleware = applyMiddleware()(createStore)
 
 ReactDOM.render(<Provider store={createStoreWithMiddleware(reducers)} > <App /></Provider>, document.getElementById('root'));
 
+
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
