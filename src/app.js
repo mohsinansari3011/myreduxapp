c140f20e1854934e0bea81435f90ed062daa0ea6
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-04-19 19:34:51 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -40,4 +40,4 @@ const mapDispatchToProps = (dispatch) => {
     },dispatch)
 }
 
-//export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
