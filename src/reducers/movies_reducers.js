8e8d84b7c4101461adf236b335c4cf7cea9b06a2
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-05-17 18:50:44 +0500

    Update movies_reducers.js

--- src/reducers/movies_reducers.js
+++ src/reducers/movies_reducers.js
@@ -1,13 +1,13 @@
 
 export default (state ={} ,action) =>{
  
-    switch (action.type) {
-        case ('MOVIES_LIST'):
-            return {...state, movies:action.payload }
-        case ('DIR_LIST'):
-            return { ...state, directors: action.payload }
-        default:
-        return state;
-    }
+    // switch (action.type) {
+    //     case ('MOVIES_LIST'):
+    //         return {...state, movies:action.payload }
+    //     case ('DIR_LIST'):
+    //         return { ...state, directors: action.payload }
+    //     default:
+    //     return state;
+    // }
 
 }
