53fc88be7a15eef6a557ddc8856d1dcee774b4e6
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-05-17 18:50:35 +0500

    Update index.js

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,8 @@
 import { combineReducers } from 'redux'
 import movies from '../reducers/movies_reducers'
 
-// const rootReducer = combineReducers({
-//     movies
-// })
+const rootReducer = combineReducers({
+    movies
+})
 
 export default rootReducer;
